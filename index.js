44c0619765586658fdd59ae61dccc7574bdca777
433MHz <krystian.izdebski1999@gmail.com>
2021-01-20 15:54:49 +0100

    Added structure of all pages

---
+++ index.js
@@ -0,0 +1,15 @@
+function relocate(buttonNum)
+{
+    switch(buttonNum) {
+        case 1:
+            window.location.href = "addPage/add.htm";
+            break;
+        case 2:
+            window.location.href = "editPage/edit.htm";
+            break;
+        case 3:
+            window.location.href = "findPage/find.htm";
+            break;
+
+      }
+}
