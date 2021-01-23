0de98839210e8b6c518fe7bcd9958c502dde585c
433MHz <krystian.izdebski1999@gmail.com>
2021-01-23 14:27:09 +0100

    top bar in showAll page

--- addPage/add.js
+++ addPage/add.js
@@ -1,12 +1,44 @@
+var choosedBox;
+
 function showBoxes()
 {
     var button = document.getElementById("boxesButtons");
 
     var html = "";
-    
+
     for(var i = 0; i < 10; i++){
-        html = html + "<input class='boxNumberOption' id='boxNumberFirst' type='button' value='csgf' onclick='showBoxes()'>";
+        html = html + "<input class='boxNumberOption' id='boxNumber"+ (i+1) +"' type='button' value='"+ "Pudełko "+ (i+1) +"' onclick='chooseBox("+(i+1)+")'>";
     }
 
     button.innerHTML = html;
+}
+
+function sendItem()
+{
+    var boxNumber;
+    var itemName;
+    var itemDesc;
+    var itemAmount;
+
+    boxNumber = choosedBox;
+    itemName = document.getElementById("itemNameText").value;
+    itemDesc = document.getElementById("itemDescTextArea").value;
+    itemAmount = document.getElementById("amountText").value;
+
+    
+}
+
+
+function chooseBox(number)
+{
+    var name = "boxNumber" + number;
+
+    var boxes = document.getElementsByClassName("boxNumberOption");
+    for(var i = 0; i < boxes.length; i++){
+        boxes[i].style.backgroundColor="#197a29";
+    }
+
+    var html = "<input class='boxNumberOption' type='button' value='"+ "Pudełko "+ number +"' onclick='showBoxes()'>";
+    var choosedBox = document.getElementById("boxesButtons").innerHTML = html;
+    choosedBox = number;
 }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function relocate(buttonNum)
             window.location.href = "addPage/add.htm";
             break;
         case 2:
-            window.location.href = "editPage/edit.htm";
+            window.location.href = "showAllPage/showAll.htm";
             break;
         case 3:
             window.location.href = "findPage/find.htm";
