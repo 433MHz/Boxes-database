0a738c7aaf1bf9bf06316f05d72ead228feeefae
433MHz <krystian.izdebski1999@gmail.com>
2021-01-27 18:32:27 +0100

    code to add data to database (not working)

--- addPage/add.js
+++ addPage/add.js
@@ -1,17 +1,39 @@
 var choosedBox;
 
-function showBoxes()
+
+function get(){
+    var object;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/count", true);
+    xhr.responseType = "json";
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    xhr.onload = function(){
+        object = xhr.response;
+        showBoxes(object.boxes);
+    };
+
+    xhr.send();
+}
+
+
+
+
+function showBoxes(number)
 {
     var button = document.getElementById("boxesButtons");
 
     var html = "";
 
-    for(var i = 0; i < 10; i++){
+    for(var i = 0; i < number; i++){
         html = html + "<input class='boxNumberOption' id='boxNumber"+ (i+1) +"' type='button' value='"+ "Pudełko "+ (i+1) +"' onclick='chooseBox("+(i+1)+")'>";
     }
 
     button.innerHTML = html;
 }
+
+
+
 
 function sendItem()
 {
@@ -23,9 +45,31 @@ function sendItem()
     boxNumber = choosedBox;
     itemName = document.getElementById("itemNameText").value;
     itemDesc = document.getElementById("itemDescTextArea").value;
-    itemAmount = document.getElementById("amountText").value;
+    itemAmount = parseInt((document.getElementById("amountText").value), 10);
 
-    
+    var objectToSend = {
+        boxNum: 1, 
+        itemName: 'Tomek', 
+        itemDesc: 'bulbulator', 
+        amount: 3
+    };
+
+    send(objectToSend);
+    function send(objectSend){
+        var object;
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://192.168.0.100:8080/add");
+        xhr.responseType = "json";
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        
+        xhr.onload = function(){
+            object = xhr.response;
+        };
+        
+        xhr.send(JSON.stringify(objectSend));
+        console.log(object);
+    }
+
 }
 
 
@@ -38,7 +82,7 @@ function chooseBox(number)
         boxes[i].style.backgroundColor="#197a29";
     }
 
-    var html = "<input class='boxNumberOption' type='button' value='"+ "Pudełko "+ number +"' onclick='showBoxes()'>";
-    var choosedBox = document.getElementById("boxesButtons").innerHTML = html;
+    var html = "<input class='boxNumberOption' type='button' value='"+ "Pudełko "+ number +"' onclick='get()'>";
+    document.getElementById("boxesButtons").innerHTML = html;
     choosedBox = number;
 }
